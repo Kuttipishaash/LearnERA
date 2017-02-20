6f1f583f3379cb4b9b394e8d7e20c12fbcfe1659
Prejith Premkumar <prajithpremg@gmail.com>
2017-02-20 23:03:20 +0530

    Recycler view + cards for contacts initial

--- app/src/main/java/com/learnera/app/ContactsActivity.java
+++ app/src/main/java/com/learnera/app/ContactsActivity.java
@@ -1,16 +1,56 @@
 package com.learnera.app;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.learnera.app.data.Contacts;
+import com.learnera.app.data.ContactsAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Prejith on 8/11/2016.
  */
 
 public class ContactsActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private List<Contacts> contactsList;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_contacts);
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view_contacts);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        contactsList = new ArrayList<>();
+        mAdapter = new ContactsAdapter(this, contactsList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        prepareData();
+
+        try {
+            Glide.with(this).load(R.drawable.placeholder).into((ImageView)findViewById(R.id.card_contact_image));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,5 +76,16 @@ public class ContactsActivity extends AppCompatActivity {
                 .setMessage(R.string.help_contacts_message)
                 .setPositiveButton(R.string.ok, null)
                 .show();
+    }
+
+    private void prepareData() {
+        Contacts c = new Contacts("Hoo", "bla", "95679", R.drawable.placeholder);
+        contactsList.add(c);
+
+        c = new Contacts("Hogro", "bntnla", "985679", R.drawable.placeholder);
+        contactsList.add(c);
+
+        c = new Contacts("Hntnoo", "blntda", "9215679", R.drawable.placeholder);
+        contactsList.add(c);
     }
 }
