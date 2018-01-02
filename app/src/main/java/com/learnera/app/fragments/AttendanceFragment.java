67bf6fc6158d3712ac1ae52fbcc07181f95a5f47
Prejith Premkumar <prajithpremg@gmail.com>
2018-01-02 19:30:18 +0300

    Cosmetic changes in syllabus

--- app/src/main/java/com/learnera/app/fragments/AttendanceFragment.java
+++ app/src/main/java/com/learnera/app/fragments/AttendanceFragment.java
@@ -2,11 +2,9 @@ package com.learnera.app.fragments;
 
 import android.app.Dialog;
 import android.app.ProgressDialog;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
