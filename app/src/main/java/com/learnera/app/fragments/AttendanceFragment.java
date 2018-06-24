d2043b0cbbab78af918936348a8ac0d331c0256f
mebinjohnson <mebinjohnson@gmail.com>
2018-06-24 21:01:14 +0400

    Reactive UI 1.0

--- app/src/main/java/com/learnera/app/fragments/AttendanceFragment.java
+++ app/src/main/java/com/learnera/app/fragments/AttendanceFragment.java
@@ -8,6 +8,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
@@ -639,6 +640,26 @@ public class AttendanceFragment extends Fragment implements AdapterView.OnItemSe
             return null;
         }
     }
+
+    public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
+        // ...
+
+        @Override
+        public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+                                   View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+            super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+                    dyUnconsumed);
+
+            if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+                child.hide();
+            } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+                child.show();
+            }
+        }
+
+        // ...
+    }
+
 
 
 }
